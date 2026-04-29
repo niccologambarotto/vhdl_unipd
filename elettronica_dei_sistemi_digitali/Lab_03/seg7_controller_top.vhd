b090200dbefbe3e14c81bac135f90b1d4d097138
NiccoloNiccolo <niccolo.gambarotto@studenti.unipd.it>
2026-04-29 13:41:17 +0200

    Creata entity e component di hex to 7seg

--- elettronica_dei_sistemi_digitali/Lab_03/seg7_controller_top.vhd
+++ elettronica_dei_sistemi_digitali/Lab_03/seg7_controller_top.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -33,17 +33,20 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity seg7_controller_top is
 	--useful generic to set the switching rate
-    --Generic(switchingRate: ?? := ??);
-
-    --Port ( ?? );
-		   
+    Generic(switchingRate: integer := 100000);
+		   port(CLK: in std_logic;
+                RST: in std_logic;
+                SW: in std_logic_vector(15 downto 0);
+                CAT: out std_logic_vector(6 downto 0);
+                An: out std_logic_vector(7 downto 0));
 end seg7_controller_top;
 
 architecture Behavioral of seg7_controller_top is
 
-    --component hex_to_7seg_decoder
-    -- ??
-	--end component;
+    component hex_to_7seg_decoder is 
+    Port ( BIN : in STD_LOGIC_VECTOR (3 downto 0);
+           SEG : out STD_LOGIC_VECTOR (6 downto 0));
+	end component;
 
 --signal counter: integer := 0;
 
